79786ce583a4373b5df25d5f3c2e80cb8328ccc0
MahmoudSfn <35277201+MahmoudSfn@users.noreply.github.com>
2019-04-22 21:38:07 +0300

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,31 +1,30 @@
 '''
-github: MahmoudSfn
-
-file description: lexer | file code analyser
+    github: MahmoudSfn
+    file description: lexer | file code analyser
+    edited at: 22/04/2019    
 '''
 import re
 import sys
 
 #define the regular expression table
-regExps = [
-        (r'[ \n\t]+', None), (r'#[^\n]*', None), (r'for\b', 'FOR'), (r'if\b', 'IF'), (r'else\b', 'ELSE'), (r'break\b', 'BREAK'), (r'while\b', 'WHILE'), 
-        (r'return\b', 'RETURN'), (r'struct\b', 'STRUCT'), (r'typedef\b', 'TYPEDEF'), (r'sizeof\b', 'SIZEOF'), (r'switch\b', 'SWITCH'), (r'case\b', 'CASE'), 
-        (r'default\b', 'DEFAULT'), (r'do\b', 'DO'), (r'void\b', 'VOID'), (r'goto\b', 'GOTO'), (r'int\b', 'INT'), (r'char\b', 'CHAR'), (r'short\b', 'SHORT'), 
-        (r'long\b', 'LONG'), (r'float\b', 'FLOAT'), (r'double\b', 'DOUBLE'), (r'signed\b', 'SIGNED'), (r'unsigned\b', 'UNSIGNED'), 
-        (r'\(', 'LPAREN'), (r'\)', 'RPAREN'), (r'\{', 'LBRACE'), (r'\}', 'RBRACE'), (r'\[', 'LBRACKET'), (r'\]', 'RBRACKET'), (r'\;', 'SEMICOLON'), 
-        (r'\:', 'COLON'), (r'\,', 'COMMA'), (r'\/\*', 'LCOMMENT'), (r'\*\/', 'RCOMMENT'), (r'\/\/(.*)', 'COMMENT'), (r'\.', 'DOT'), (r'\=\=', 'EQ'), 
-        (r'\=', 'ASSIGN'), (r'\+\+', 'ADDADD'), (r'\+\=', 'ADDEQ'), (r'\+', 'ADD'), (r'\-\-', 'SUBSUB'), (r'\-\=', 'SUBEQ'), (r'\-', 'SUB'), (r'\*', 'MUL'), 
-        (r'\/', 'DIV'), (r'\!\=', 'NEQ'), (r'\|\|', 'DBAR'), (r'\<', 'LT'), (r'\<\=', 'LTE'), (r'\>', 'GT'), (r'\>\=', 'GTE'), (r'\&', 'AMPERSAND'), 
-        (r'\&\&', 'DAMPERSAND'), (r'\#', 'SHARP'), (r'[a-zA-Z]\w*', 'IDENTIFIER'), (r'\d+\.\d+', 'FLOAT_LIT'), (r'\d+', 'INTEGER_LIT'),
-        (r'\"[^\"]*\"', 'STRING_LIT'), (r'\'[^\"]*\'', 'CHAR_LIT'), (r'\w+(\.\w+)+', 'SELECTED_NAME') 
+regExps = [    
+    (r'\=', 'EqualSign'), 
+    (r'[a-zA-Z]\w*', 'variable'),
+    (r'\d+\.\d+', 'FloatNumber'),
+    (r'\d+', 'IntegerNumber') 
 ]
-# open the text file
+# open the code file
 file = open("test.txt","r") 
+LineNumber = 0
+WordInLine = 0
 
 for line in file:
+    LineNumber = LineNumber + 1
     line = line.strip("\n") # to remove the new line (\n) from the line
     print("\nin line : \" " + line + " \" ")
-    for code in line.split():
+    WordInLine = 0
+    for code in line.split(): # eachcode is a word
+        WordInLine = WordInLine + 1 # to get the position of each word in the line to display more detail about the error
         # parcourir le tableau et comparer
         found = False
         for pattern in regExps:
@@ -37,12 +36,12 @@ for line in file:
                     count = 1
                 else:
                     meanning = x
-            if re.search(regexp, code):
-                print(code + " match to " + meanning)
-                found = True
-                break
+            if re.search(regexp, code) is not None: # 
+                print(code + " match to " + str(meanning)) # displaying the code and signification in regular expression table
+                found = not False
+                break # stop running the table when its compatible to a reg exp
         if found != True:
-            print("No match!")
-            found = False
+            print("error! position: [ line: " + str(LineNumber) + " ; word: \' " + code + " \'; word position" + str(WordInLine) + " ] ")
+            found = not True
 
 # print(file.read())
